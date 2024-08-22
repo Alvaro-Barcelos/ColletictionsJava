d8ff2ed6ac723ac07d98010cdac98594b994e1f1
Álvaro Barcelos <alvaropassalto@gmail.com>
2024-08-22 11:00:24 -0300

    Treinando collections

---
+++ src/list/ExerciciosPropostos.java
@@ -0,0 +1,73 @@
+package list;
+
+import java.util.ArrayList;
+import java.util.Iterator;
+import java.util.List;
+import java.util.Scanner;
+
+public class ExerciciosPropostos {
+
+    public static void main(String[] args) {
+
+        Scanner input = new Scanner(System.in);
+
+
+        List<Integer> temperatura = new ArrayList<>();
+        int temperaturaMes;
+        int soma = 0;
+
+
+        for (int i = 1; i <= 6; i++) {
+            System.out.println("Qual a temperatura do mês - " + i);
+            temperaturaMes = input.nextInt();
+            temperatura.add(temperaturaMes);
+            soma += temperaturaMes; //soma para calcular a media
+            input.nextLine();
+        }
+
+        //OUTRA FORMA DE REALIZAR A SOMA
+        //Iterator<Integer> iterator = temperatura.iterator();
+        //int sum = 0;
+        //while (iterator.hasNext()){
+        //    Integer next = iterator.next();
+        //    sum += next;
+        //}
+
+        System.out.println("TEMPERATURAS");
+        System.out.println(temperatura);
+        System.out.println("MEDIA SEMESTRAL");
+        System.out.println((soma / temperatura.size()));
+
+        System.out.println("TEMPERATURAS ACIMA DA MEDIA");
+        for (int temperaturas : temperatura) {
+            if (temperaturas > (soma / temperatura.size())) {
+                int index = temperatura.indexOf(temperaturas);
+                switch (index) {
+                    case 0:
+                        System.out.println(index + 1 + " - JANEIRO: " + temperaturas + " oC");
+                        break;
+                    case 1:
+                        System.out.println(index + 1 + " - Fevereiro: " + temperaturas + " oC");
+                        break;
+                    case 2:
+                        System.out.println(index + 1 + " - Março: " + temperaturas + " oC");
+                        break;
+                    case 3:
+                        System.out.println(index + 1 + " - Abril: " + temperaturas + " oC");
+                        break;
+                    case 4:
+                        System.out.println(index + 1 + " - Maio: " + temperaturas + " oC");
+                        break;
+                    case 5:
+                        System.out.println(index + 1 + " - Junho: " + temperaturas + " oC");
+                        break;
+                }
+            }
+
+
+
+
+        }
+
+    }
+}
---
+++ src/list/Main.java
@@ -0,0 +1,102 @@
+package list;
+
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) {
+
+        List<Double> notas = new ArrayList<>();
+
+        notas.add(7.0);
+        notas.add(8.5);
+        notas.add(9.3);
+        notas.add(5.0);
+        notas.add(7.0);
+        notas.add(0.0);
+        notas.add(3.6);
+
+        System.out.println(notas); //Pode se utilizar o toString
+        System.out.println("--------------------------------------------------");
+        System.out.println("Posição da nota 5:" + notas.indexOf(5d));
+
+        System.out.println("--------------------------------------------------");
+        System.out.println("Adicionar elemento na posição 4");
+        notas.add(4, 8d);
+        System.out.println(notas);
+
+        System.out.println("--------------------------------------------------");
+        System.out.println("Substituir nota 5 pela nota 6");
+        notas.set(notas.indexOf(5d),6.0 );
+        System.out.println(notas);
+
+        System.out.println("--------------------------------------------------");
+        System.out.println("Confira se a nota 5.0 esta na lista: " + notas.contains(9.3));
+
+        System.out.println("--------------------------------------------------");
+        System.out.println("Exiba todas as notas na ordem que foi informado: ");
+        for (Double nota : notas) System.out.println(nota);
+
+        System.out.println("--------------------------------------------------");
+        System.out.println("Exiba a terceira nota adicionada: " + notas.get(2));
+
+        System.out.println("--------------------------------------------------");
+        System.out.println("Exiba a menor nota: " + Collections.min(notas));
+
+        System.out.println("--------------------------------------------------");
+        System.out.println("Exiba a maior nota: " + Collections.max(notas));
+
+        System.out.println("--------------------------------------------------");
+        Iterator<Double> iterator = notas.iterator();
+        Double soma = 0d;
+        while (iterator.hasNext()){ //Iterator tem algum numero depois?
+            Double next = iterator.next(); //Coloco o elemento dentro da variavel next
+            soma += next; // Coloco agora em soma pra gerar um acumulador da soma de todas as notas
+        }
+        System.out.println("Exiba a soma dos valores: " + soma);
+
+        System.out.println("--------------------------------------------------");
+        System.out.println("Media das notas: " + (soma/notas.size()));
+
+        System.out.println("--------------------------------------------------");
+        System.out.println("Remova a nota 0: ");
+        notas.remove(0.0);
+        System.out.println(notas);
+
+        System.out.println("--------------------------------------------------");
+        System.out.println("Remova a posição 0: ");
+        notas.remove(0);
+
+        System.out.println("--------------------------------------------------");
+        System.out.println("Remova as notas menores que 7:");
+        Iterator<Double> iterator1 = notas.iterator();
+        while (iterator1.hasNext()){
+            Double next = iterator1.next();
+            if (next < 7) iterator1.remove();
+        }
+        System.out.println(notas);
+
+        System.out.println("--------------------------------------------------");
+        System.out.println("Apague toda a lista:");
+        notas.clear();
+        System.out.println(notas);
+
+        System.out.println("--------------------------------------------------");
+        System.out.println("Confira se a lista esta vazia: " + notas.isEmpty());
+
+        System.out.println("--------------------------------------------------");
+        System.out.println("--------------------------------------------------");
+        List<Double> notas2 = new LinkedList<>();
+        notas2.add(7.0);
+        notas2.add(8.5);
+        notas2.add(9.3);
+        notas2.add(5.0);
+        notas2.add(7.0);
+        notas2.add(0.0);
+        notas2.add(3.6);
+        System.out.println(notas2);
+
+        System.out.println(notas2.getFirst());
+        notas2.removeFirst();
+        System.out.println(notas2);
+    }
+}
